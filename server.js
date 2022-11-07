105371df5cc14a3504be738272de9a15bd26769e
robinbaud <72597879+robinbaud@users.noreply.github.com>
2022-11-07 23:25:20 +0100

    change tout

--- server.js
+++
@@ -1,9 +0,0 @@
-const http = require("http");
-
-const server = http.createServer((req, res) => {
-  console.log("request done");
-});
-
-server.listen(3001, () => {
-  console.log("connection faite");
-});
